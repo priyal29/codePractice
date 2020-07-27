bc519ae0e0d21cf16abffd855a56131e9475ef58
priyal.desai <priyal.desai29@gmail.com>
2020-07-27 11:24:43 +0530

    BST and likedList problems

--- arrays/missingNum.cpp
+++ arrays/missingNum.cpp
@@ -3,6 +3,8 @@
 
 using namespace std;
 
+#if 0
+//early version
 int main()
 {
 	int n, a[20];
@@ -19,6 +21,35 @@ int main()
 	cout << "total is = " << sum << "\n";
 	int missingNum = seriesSum-sum;
 	cout << "result = " << missingNum << "\n";
+	return 0;
+}
+#endif
+
+int findNum(int arr[], int n)
+{
+    int low = 0, high = n-1, mid = 0;
+    while(low <= high){
+        mid = low + (high-low)/2;
+        if(arr[mid]-mid > 1)
+            high = mid+1;
+        else
+            low = mid;
+    }
+    return low;
+}
+
+int main() {
+	int n;
+//	cout << "enter no. of elements" << endl;
+//	cin >> n;
+	int arr[] = {1,2,3,4,6,7,8};
+	n = sizeof(arr)/sizeof(arr[0]);
+//	cout << "elements" << endl;
+//	for(int i=0; i<n; i++){
+//	    cin >> arr[i];
+//	}
+	int num = findNum(arr, n);
+	cout << "missing number is " << arr[num]-1 << endl;
 	return 0;
 }
 
